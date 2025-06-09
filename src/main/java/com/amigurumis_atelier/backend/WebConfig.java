fb81beb69eae981f78ec9759febd20de6e690ff1
andres1988 <a.canorave@gmail.com>
2025-06-09 12:19:34 -0500

    FEAT: Añadir ruta de front en webConfig

--- src/main/java/com/amigurumis_atelier/backend/WebConfig.java
+++ src/main/java/com/amigurumis_atelier/backend/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501")
+                .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501, https://main.d2pyfqpvl5zw8r.amplifyapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
